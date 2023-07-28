168683cbfbc3f080d04d1105d2979aa4a8162a3f
Anastasia Polivanova <lyaminana@gmail.com>
2023-07-29 00:28:16 +0300

    решила задачу 64

---
+++ hw1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 64: Задайте значение m n. 
+//Напишите программу, которая выведет 
+//все натуральные числа в промежутке от m до n. Выполнить с помощью рекурсии.
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Введите число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+string PrintNumbers(int firstNumber, int secondNumber)
+{
+    if (firstNumber == secondNumber) return firstNumber.ToString();
+    return firstNumber + " , " + PrintNumbers(firstNumber + 1, secondNumber);
+}
+
+Console.WriteLine($"числа от {number1} до {number2} = {PrintNumbers(number1, number2)}");
